ddd36a7c2ff97cb9466e159d1add59cd6167d203
magmabyte <[omitted]>
2020-02-02 17:20:57 +0300

    Add feed component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogFeedComponent } from './blog-feed/blog-feed.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/feed', pathMatch: 'full' },
+  { path: 'feed', component: BlogFeedComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

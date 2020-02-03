ef72bdfce130ea43077ac0f2e1580d32b94b4d8c
magmabyte <[omitted]>
2020-02-03 11:45:17 +0300

    Improve styling of quotes in markdown

--- src/app/markdown.ts
+++ src/app/markdown.ts
@@ -4,8 +4,18 @@ export function markedOptionsFactory(): MarkedOptions {
   const renderer = new MarkedRenderer();
 
   renderer.image = (href: string, title: string, text: string) => {
-    return '<center><img src="' + href + '" style="max-width: 300px; max-height: 300px;"></center>';
+    return '<center> \
+      <img src="' + href + '" style="max-width: 300px; max-height: 300px;"> \
+      </center>';
   };
+
+  renderer.blockquote = (quote: string) => {
+    console.log(quote);
+    return '<div style="display: flex; flex-direction: row; justify-content: start"> \
+    <div style="min-height: 1px; min-width: 2px; background: rgb(200, 200, 200); margin-right: 5px"></div>\
+    <div style="color: grey">' + quote + '</div>\
+    </div>';
+  }
 
   return {
     renderer
